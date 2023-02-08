c42e07a2bb6ea5f5a619c6711708ccd825654348
denisBFT <d.isangulov@bftcom.com>
2023-02-08 22:36:15 +0300

    docker compose

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,8 @@ def browser():
     options = webdriver.ChromeOptions()
     options.add_argument('--window-size=1920,1080')
     options.add_argument("--headless")
-    # browser = webdriver.Remote(command_executor="http://selenium__standalone-chrome:4444/wd/hub", options=options)
-    browser = webdriver.Chrome(executable_path='chromedriver', options=options)
+    browser = webdriver.Remote(command_executor="http://selenium__standalone-chrome:4444/wd/hub", options=options)
+    # browser = webdriver.Chrome(executable_path='chromedriver', options=options)
     # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
     yield browser
