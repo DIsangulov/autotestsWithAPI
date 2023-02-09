b0fb39fb677931b53e7eeedb9a3c34982bbabfc1
denisBFT <d.isangulov@bftcom.com>
2023-02-09 17:41:31 +0300

    docker compose

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -5,6 +5,8 @@ from pages.UI._0_Auth.auth_page import AuthPage
 
 class TestAuth:
     def test_valid_auth(self, browser):
-        link = "https://10.130.0.22"
+        # link = "https://10.130.0.22"
+        addr = "https://10.130.0.22"
+        link = addr
         page = AuthPage(browser, link)
         page.open()
