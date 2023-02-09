b9e7d7b055214c150cab45202f7b8b0d5000ce39
denisBFT <d.isangulov@bftcom.com>
2023-02-09 13:13:49 +0300

    docker compose

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from seleniumwire import webdriver
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument('--window-size=1920,1080')
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     browser = webdriver.Remote(command_executor="http://172.18.0.2:5555/wd/hub", options=options)
     # browser = webdriver.Chrome(executable_path='chromedriver', options=options)
     # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
