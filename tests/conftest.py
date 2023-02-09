fd0a8c7198c1d4ccfa60effa4e5d52f8dadc2def
denisBFT <d.isangulov@bftcom.com>
2023-02-09 17:47:45 +0300

    docker compose

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,14 +6,13 @@ from seleniumwire import webdriver
 def browser():
     options = webdriver.ChromeOptions()
     # options.add_argument('--window-size=1920,1080')
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--start-maximized')
-
     browser = webdriver.Remote(command_executor="http://172.17.0.2:4444/wd/hub", options=options)
-    browser = webdriver.Chrome(executable_path='chromedriver', options=options)
+    # browser = webdriver.Chrome(executable_path='chromedriver', options=options)
     # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
     yield browser
