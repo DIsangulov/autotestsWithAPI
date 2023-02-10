48c1f782378d2f554a93c7c47926fb7c3b8cda0f
denisBFT <d.isangulov@bftcom.com>
2023-02-10 15:36:12 +0300

    two test jobs

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,12 +1,14 @@
 import time
 
+import allure
+
 from pages.UI._0_Auth.auth_page import AuthPage
 
 
+@allure.feature("UI - Страница авторизации")
 class TestAuth:
+    @allure.story("Авторизация")
     def test_valid_auth(self, browser):
-        # link = "https://10.130.0.22"
-        addr = "https://10.130.0.22"
-        link = addr
+        link = "https://10.130.0.22"
         page = AuthPage(browser, link)
         page.open()
