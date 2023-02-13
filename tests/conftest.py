f65c2e00f8a35282f740dec05aa2ba46fba84936
denisBFT <d.isangulov@bftcom.com>
2023-02-13 14:48:27 +0300

    image yandex

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,8 +11,10 @@ def browser():
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--start-maximized')
+    binary_yandex_driver_file = 'yandexdriver.exe'  # path to YandexDriver
     # browser = webdriver.Remote(command_executor="http://172.17.0.2:4444/wd/hub", options=options)
-    browser = webdriver.Chrome(executable_path='chromedriver', options=options)
+    # browser = webdriver.Chrome(executable_path='chromedriver', options=options)
+    browser = webdriver.Chrome(binary_yandex_driver_file, options=options)
     # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
     yield browser
