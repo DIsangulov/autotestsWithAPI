b31aa7b22b4bc5625f51007e77e3b623a3df8fdc
denisBFT <d.isangulov@bftcom.com>
2023-02-14 15:42:43 +0300

    image yandex

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ def browser():
     # browser = webdriver.Chrome(executable_path='chromedriver', options=options)
     # options.binary_location = '/Applications/Yandex.app/Contents/MacOS/Yandex'  # для локального запуска яндекс браузера
     options.binary_location = '/usr/bin/yandex-browser'  # для запуска яндекс браузера из Gitlab
-    browser = webdriver.Chrome(chrome_options=options, executable_path='chromedriver.exe')
+    browser = webdriver.Chrome(chrome_options=options, executable_path='chromedriver.exe')  # локальный драйвер
     # browser = webdriver.Chrome(executable_path='yandexdriver', options=options)
     # browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
