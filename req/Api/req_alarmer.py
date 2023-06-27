da06734c09c31682acbd096a9998c1dc5b53dae9
s.yezhov <s.yezhov@ngrsoftlab.ru>
2023-06-27 18:59:29 +0300

    ref: req_alarmer.py > небольшие правки под шаблон API_AUTO_TEST_

--- req/Api/req_alarmer.py
+++ req/Api/req_alarmer.py
@@ -2,6 +2,7 @@ from req.Helpers.base_req import BaseReq
 from resourses.credentials import DpQaa
 
 _QA_SPAM_EMAIL = "s.yezhov@ngrsoftlab.ru"
+API_AUTO_TEST_ = "API_AUTO_TEST_"
 
 
 class Alarmer(BaseReq):
@@ -23,8 +24,7 @@ class Alarmer(BaseReq):
             "id": 5590483  # FIXME: magic?
         }
         header = {'token': self.token}
-        resp = self.sess.post(f"{self.host}/back/dp.alarmer/notification/read/admin", headers=header, json=body,
-                              verify=False)
+        resp = self.sess.post(f"{self.host}/back/dp.alarmer/notification/read/admin", headers=header, json=body, verify=False)
         return resp
 
     def alarmer_notification_settings_admin_get(self):
@@ -44,8 +44,7 @@ class Alarmer(BaseReq):
             "notify_duration": 1
         }
         header = {'token': self.token}
-        resp = self.sess.post(f"{self.host}/back/dp.alarmer/notification/settings/common", headers=header, json=body,
-                              verify=False)
+        resp = self.sess.post(f"{self.host}/back/dp.alarmer/notification/settings/common", headers=header, json=body, verify=False)
         return resp
 
     def alarmer_notification_settings_user_get(self):
@@ -60,8 +59,7 @@ class Alarmer(BaseReq):
             "user_id": 1
         }
         header = {'token': self.token}
-        resp = self.sess.post(f"{self.host}/back/dp.alarmer/notification/settings/userone", headers=header, json=body,
-                              verify=False)
+        resp = self.sess.post(f"{self.host}/back/dp.alarmer/notification/settings/userone", headers=header, json=body, verify=False)
         return resp
 
     def alarmer_notification_settings_type_post(self):  # мегастранный запрос и большой
@@ -70,63 +68,60 @@ class Alarmer(BaseReq):
         }
         header = {'token': self.token}
         # FIXME: не тот EP
-        resp = self.sess.post(f"{self.host}/back/dp.alarmer/notification/settings/user", headers=header, json=body,
-                              verify=False)
+        resp = self.sess.post(f"{self.host}/back/dp.alarmer/notification/settings/user", headers=header, json=body, verify=False)
         return resp
 
     def alarmer_notification_settings_user_all_get(self):
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.alarmer/notification/user/all", headers=header,
-                             verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.alarmer/notification/user/all", headers=header, verify=False)
         return resp
 
     def alarmer_notification_user_get(self):
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.alarmer/notification/user", headers=header,
-                             verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.alarmer/notification/user", headers=header, verify=False)
         return resp
 
     def alarmer_send_invitation_post(self):
+        # https://tasks.ngrsoftlab.ru/browse/DAT-5291
         body = {
             # "link": "https://10.130.0.22/",  # нужно?
             # "msg": "TestAPI",                # нужно?
             "to": _QA_SPAM_EMAIL,
-            "user_id": 7741  # FIXME: могу отправлять сообщения от другого имени?
+            "user_id": 4870     # FIXME: могу отправлять сообщения от другого имени?
         }
         header = {'token': self.token}
-        resp = self.sess.post(f"{self.host}/back/dp.alarmer/send_invitation", headers=header, json=body,
-                              verify=False)
+        resp = self.sess.post(f"{self.host}/back/dp.alarmer/send_invitation", headers=header, json=body, verify=False)
         return resp
 
     def alarmer_send_invitations_post(self):
+        # https://tasks.ngrsoftlab.ru/browse/DAT-5291
         body = {
             # "link": "https://10.130.0.22/",     # нужно?
             # "msg": "TestAPI",                   # нужно?
             "to": [
                 _QA_SPAM_EMAIL
             ],
-            "user_id": 7741
+            "user_id": 4870     # FIXME: могу отправлять сообщения от другого имени?
         }
         header = {'token': self.token}
-        resp = self.sess.post(f"{self.host}/back/dp.alarmer/send_invitations", headers=header, json=body,
-                              verify=False)
+        resp = self.sess.post(f"{self.host}/back/dp.alarmer/send_invitations", headers=header, json=body, verify=False)
         return resp
 
     def alarmer_send_msg_post(self):
         body = {
-            "description": "TestApiAlarmer",
+            "topic": API_AUTO_TEST_ + "topic",
+            "message": API_AUTO_TEST_ + "message",
+            "name": API_AUTO_TEST_ + "name",
+            "description": API_AUTO_TEST_ + "alarmer_desc",
             "disable_tls": False,
             "host": "NGR-Exchange01.ngrsoftlab.ru",
-            "message": "TestAPI_message",
-            "name": "TestAPI_name",
             "port": 587,
             "protocol": "smpt",
-            "send_user": DpQaa.USER,
-            "psw": DpQaa.PASS,
-            # "to": "d.isangulov@ngrsoftlab.ru",
             "to": _QA_SPAM_EMAIL,
-            "topic": "TestAPI_topic",
-            "user": DpQaa.USER
+            # "send_user": DpQaa.USER,
+            "send_user": "Владимир Даль",       # FIXME: Имя отправителя в полученном сообщении
+            "psw": DpQaa.PASS,      # пароль для аунтефикации
+            "user": DpQaa.USER      # имя для аунтефикации
         }
         header = {'token': self.token}
         resp = self.sess.post(f"{self.host}/back/dp.alarmer/send_msg", headers=header, json=body, verify=False)
