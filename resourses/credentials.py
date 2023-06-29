cdc964f8004cc9c1a97b4fbc312a9c3e7efbbe41
s.yezhov <s.yezhov@ngrsoftlab.ru>
2023-06-29 12:31:23 +0300

    add: base_req.py > get_db_id_by_name

--- resourses/credentials.py
+++ resourses/credentials.py
@@ -6,3 +6,9 @@ class DpQaa:
 class DpQaaLocal:
     USER = "dataplan_qaa_local"
     PASS = "so@you_came_back_to@me_again@@DdwIf991"
+
+
+class DbName:
+    picker_tables = "picker_tables"     # FIXME: возможно, убрать зависимости
+    API_TEST_DB1 = "API_TEST_DB1"
+    API_TEST_DB2 = "API_TEST_DB2"
