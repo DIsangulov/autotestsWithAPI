58f3d15bc6b6c470019847313d7a6c5a006e15ce
s.yezhov <s.yezhov@ngrsoftlab.ru>
2023-06-30 17:28:48 +0300

    ref: req_rm_cook.py > swagger соответствие

--- req/Api/req_rm_cook.py
+++ req/Api/req_rm_cook.py
@@ -23,27 +23,32 @@ class RmCook(BaseReq):
         return int(_rm_group_info['id'])
 
     def rm_cook_active_directory_groups_get(self):
+        """process GET req to get AD groups list."""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/active_directory/groups", headers=header, verify=False)
         return resp
 
     def rm_cook_active_directory_groups_id_get(self):
+        """process GET req to get AD group by id"""
         _rm_group_id = self._get_random_rm_group_id()
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/active_directory/groups/" + str(_rm_group_id), headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/active_directory/groups/{_rm_group_id}", headers=header, verify=False)
         return resp
 
     def rm_cook_active_directory_state_get(self):
+        """process GET req to get AD state stats/metrics"""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/active_directory/state", headers=header, verify=False)
         return resp
 
     def rm_cook_active_directory_top_groups_get(self):
+        """process GET req to get AD top groups list"""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/active_directory/top_groups", headers=header, verify=False)
         return resp
 
     def rm_cook_active_directory_top_users_get(self):
+        """process GET req to get AD top users list"""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/active_directory/top_users", headers=header, verify=False)
         return resp
@@ -58,43 +63,57 @@ class RmCook(BaseReq):
         """process GET req to get AD user by ID"""
         _rm_user_id = self._get_random_rm_user_id()
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/active_directory/users/" + str(_rm_user_id), headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/active_directory/users/{_rm_user_id}", headers=header, verify=False)
         return resp
 
+    # FIXME: mark.skip
     def rm_cook_calculation_start_calc_id_post(self):
+        """process POST req to start rm calculation"""
+        calc_id = 1
         header = {'token': self.token}
-        resp = self.sess.post(f"{self.host}/back/dp.rm_cook/calculation/start/1", headers=header, verify=False)
+        resp = self.sess.post(f"{self.host}/back/dp.rm_cook/calculation/start/{calc_id}", headers=header, verify=False)
         return resp
 
     def rm_cook_rm_logs_last_get(self):
+        """process GET req to get the last RM calculation log"""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/rm/logs/last", headers=header, verify=False)
         return resp
 
     def rm_cook_rm_recommendations_get(self):
+        """process GET req to get RoleMining recommendations"""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/rm/recommendations", headers=header, verify=False)
         return resp
 
     def rm_cook_rm_roles_id_alias_post(self):
+        """
+        process POST req to set role alias. calc_time parameter is optional.
+        If it's omitted, the last role model timestamp is used.
+        """
+        _id = 0         # FIXME: role_id?
         header = {'token': self.token, 'ui': str(2)}
         data = {"alias": "Роль 0_изменено", "id": 0}
-        resp = self.sess.post(f"{self.host}/back/dp.rm_cook/rm/roles/0/alias", headers=header, json=data, verify=False)
+        resp = self.sess.post(f"{self.host}/back/dp.rm_cook/rm/roles/{_id}/alias", headers=header, json=data, verify=False)
         return resp
 
+    # FIXME: /back/dp.rm_cook/rm/roles/{id}/alias/{ts}      << без {ts} писания нет
     def rm_cook_rm_roles_id_alias_get(self):
+        _id = 0
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/rm/roles/0/alias", headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/rm/roles/{_id}/alias", headers=header, verify=False)
         return resp
 
     def rm_cook_rm_status_get(self):
+        """process GET req to get RM calculation status"""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/rm/status", headers=header, verify=False)
         return resp
 
     def rm_cook_role_model_result_export_role_model_to_excel_post(self):
+        """process POST req to export role model to Excel file from rm_ml"""
         body = {
-            "groups_db": "picker_tables",
+            "groups_db": "picker_tables",       # FIXME:
             "groups_table": "ad_groups_ngr_2",
             "source": 0,
             "users_db": "picker_tables",
@@ -104,71 +123,94 @@ class RmCook(BaseReq):
         resp = self.sess.post(f"{self.host}/back/dp.rm_cook/role_model/result/export_role_model_to_excel", headers=header, json=body, verify=False)
         return resp
 
-    # FIXME: role_id == 0; кейс проходит
     def rm_cook_role_model_result_groups_by_role_id_get(self):
+        """process GET req to get full list of rm group by role id"""
+        _role_id = 0        # FIXME: role_id == 0; кейс проходит
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/groups_by_role/0", headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/groups_by_role/{_role_id}", headers=header, verify=False)
         return resp
 
-    # FIXME: role_id == 0;
     def rm_cook_role_model_result_resources_by_role_id_get(self):
+        """process GET req to get full list of rm resource by role id"""
+        _role_id = 0            # FIXME: role_id == 0;
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/resources_by_role/0", headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/resources_by_role/{_role_id}", headers=header, verify=False)
         return resp
 
     def rm_cook_role_model_result_roles_by_source_source_id_get(self):
+        """process GET req to get full list of rm roles by source id"""
+        _source_id = 0
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/roles_by_source/0", headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/roles_by_source/{_source_id}", headers=header, verify=False)
         return resp
 
-    # FIXME: role_id == 0; кейс проходит
     def rm_cook_role_model_result_source_source_id_users_by_role_role_id_get(self):
+        """process GET req to get full list of rm user by role id"""
+        _source_id = 0          # FIXME: source_id == 0
+        _role_id = 0            # FIXME: role_id == 0; кейс проходит
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/source/0/users_by_role/0", headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/source/{_source_id}/users_by_role/{_role_id}", headers=header, verify=False)
         return resp
 
-    # FIXME: role_id == 0
     def rm_cook_role_model_result_table_role_role_id_resources_by_user_user_id_get(self):
+        """process GET req to get full list of rm resource by username id and role id"""
+        _role_id = 0            # FIXME: role_id == 0
         _rm_user_id = self._get_random_rm_user_id()
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/table/role/0/resources_by_user/" + str(_rm_user_id), headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/table/role/{_role_id}/resources_by_user/{_rm_user_id}", headers=header, verify=False)
         return resp
 
-    # FIXME: role_id == 0
     def rm_cook_role_model_result_table_role_role_id_users_by_resource_resource_id_get(self):
+        """process GET req to get full list of rm user by resource name and role id"""
+        _role_id = 0                # FIXME: role_id == 0
+        _resource_id = 0            # FIXME: resource_id == 0
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/table/role/0/users_by_resource/0", headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/table/role/{_role_id}/users_by_resource/{_resource_id}", headers=header, verify=False)
         return resp
 
-    # FIXME: role_id == 0
-    def rm_cook_role_model_result_form_role_role_id_groups_by_user_user_id_get(self):
-        header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/table/role/0/users_by_group/3909688664", headers=header, verify=False)
-        return resp
-
-    # FIXME: role_id == 0
     def rm_cook_role_model_result_form_role_role_id_users_by_group_user_id_get(self):
+        """process GET req to get full list of rm group by username and role id"""
+        _form = "table"     # FIXME: какие ещё бывают
+        _role_id = 0        # FIXME: role_id == 0
+        _user_id = 0        # old: 1996248437
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/table/role/0/groups_by_user/1996248437", headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/{_form}/role/{_role_id}/groups_by_user/{_user_id}", headers=header, verify=False)
+        return resp
+
+    def rm_cook_role_model_result_form_role_role_id_groups_by_user_user_id_get(self):
+        """process GET req to get full list of rm user by group name and role id"""
+        _form = "table"             # FIXME: какие ещё бывают
+        _role_id = 0                # FIXME:
+        _group_id = 3909688664      # FIXME:
+        header = {'token': self.token}
+        resp = self.sess.get(f"{self.host}/back/dp.rm_cook/role_model/result/{_form}/role/{_role_id}/users_by_group/{_group_id}", headers=header, verify=False)
         return resp
 
     def rm_cook_settings_calc_get(self):
+        """process GET req to get current RM calculations settings."""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/settings/calc", headers=header, verify=False)
         return resp
 
     def rm_cook_settings_calc_put(self):
-        body = {"roles_num": 20}
+        """
+        process PUT req to set new RM calculations settings.
+        only roles_num is being set at the time
+        """
+        _roles_num = 20
+        body = {"roles_num": _roles_num}
         header = {'token': self.token}
         resp = self.sess.put(f"{self.host}/back/dp.rm_cook/settings/calc", headers=header, json=body, verify=False)
         return resp
 
     def rm_cook_settings_mailings_get(self):
+        """process GET req to get current syslog-mailing-alert RM settings"""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/settings/mailings", headers=header, verify=False)
         return resp
 
     def rm_cook_settings_mailings_post(self):
+        """process POST req to set new syslog-mailing-alert RM settings"""
         body = {"destinations": [
             {"email": "",
              "syslog_host": SYSLOG_HOST,
@@ -188,12 +230,13 @@ class RmCook(BaseReq):
         return resp
 
     def rm_cook_settings_sources_get(self):
+        """process GET req to get current RM sources settings"""
         header = {'token': self.token}
         resp = self.sess.get(f"{self.host}/back/dp.rm_cook/settings/sources", headers=header, verify=False)
         return resp
 
     def rm_cook_settings_sources_post(self):
-        # db_picker_tables = self._id_picker_tables_get()
+        """process POST req to set RM sources settings"""
         db_picker_tables = self.get_db_id_by_name(DbName.picker_tables)
         body = [{"db_name": "picker_tables", "db_id": db_picker_tables, "source_type": 1, "table_name": "ad_groups_ngr"},
                 {"db_name": "picker_tables", "db_id": db_picker_tables, "source_type": 2, "table_name": "ad_users_ngr"}]
