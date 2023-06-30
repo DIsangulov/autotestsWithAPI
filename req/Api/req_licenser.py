5c8da37a4157173172a37e8c88c750fc2811c80d
s.yezhov <s.yezhov@ngrsoftlab.ru>
2023-06-30 12:57:39 +0300

    ref: req_licenser.py > swagger соответствие

--- req/Api/req_licenser.py
+++ req/Api/req_licenser.py
@@ -4,6 +4,7 @@ from req.Helpers.base_req import BaseReq
 class Licenser(BaseReq):
 
     def licenser_activate_post(self):  # кидаем кривой ключ и ждем что его отобъет
+        """process POST req for activating license with text key"""
         body = {
             "key": "123-456-789"
         }
@@ -11,7 +12,12 @@ class Licenser(BaseReq):
         resp = self.sess.post(f"{self.host}/back/dp.licenser/activate", headers=header, json=body, verify=False)
         return resp
 
+    # TODO: [POST] /back/dp.licenser/file_activate
+
     def licenser_license_info_get(self):
+        """process GET req for getting current license info"""
         header = {'token': self.token, 'skey': "ANGARA"}
         resp = self.sess.get(f"{self.host}/back/dp.licenser/license_info", headers=header, verify=False)
         return resp
+
+    # TODO: [POST] /back/dp.licenser/set_company
