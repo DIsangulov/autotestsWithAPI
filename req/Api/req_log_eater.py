4378e894ae6257d1ac0959e190b70bd942af6bf7
s.yezhov <s.yezhov@ngrsoftlab.ru>
2023-06-30 12:59:51 +0300

    ref: req_log_eater.py > swagger соответствие

--- req/Api/req_log_eater.py
+++ req/Api/req_log_eater.py
@@ -4,6 +4,8 @@ from req.Helpers.base_req import BaseReq
 class LogEater(BaseReq):
 
     def log_eater_audit_users_days_get(self):
+        """process GET req http handler for getting path to file with user audit logs for "days" days count"""
+        days = "1"
         header = {'token': self.token}
-        resp = self.sess.get(f"{self.host}/back/dp.log_eater/audit/users/1", headers=header, verify=False)
+        resp = self.sess.get(f"{self.host}/back/dp.log_eater/audit/users/{days}", headers=header, verify=False)
         return resp
